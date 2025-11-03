9295a6ebd08a86633a4e4fafbc94521bf5bd41dd
HarryDang <danghuuphuc001@gmail.com>
2025-11-03 21:24:14 +0700

    feat: post resolver

--- src/posts/post.service.ts
+++ src/posts/post.service.ts
@@ -3,26 +3,56 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PostRepository } from './post.repository';
 import { Post, Prisma } from 'generated/prisma/client';
+import { PaginationParams, PaginationResult } from 'src/common/base.repository';
 
 @Injectable()
 export class PostsService {
   constructor(private readonly postRepository: PostRepository) {}
 
-  async findAll(): Promise<Post[]> {
-    return await this.postRepository.findAll();
+  async findAll(params?: Prisma.SelectSubset<any, any>): Promise<Post[]> {
+    return await this.postRepository.findAll(params);
+  }
+
+  async findBySlug(slug: string): Promise<Post | null> {
+    return await this.postRepository.findBySlug(slug);
+  }
+
+  async findPriorityPosts(limit: number = 10): Promise<Post[]> {
+    return await this.postRepository.findPriorityPosts(limit);
+  }
+
+  async searchPosts(
+    searchTerm: string,
+    params: PaginationParams,
+  ): Promise<PaginationResult<Post>> {
+    return await this.postRepository.searchPosts(searchTerm, params);
+  }
+  async findPublishedPosts(
+    params: PaginationParams,
+  ): Promise<PaginationResult<Post>> {
+    return await this.postRepository.findPublishedPosts(params);
   }
 
   async findById(id: number): Promise<Post | null> {
     return await this.postRepository.findById(id);
   }
 
+  async postPaginated(
+    params: PaginationParams,
+  ): Promise<PaginationResult<Post>> {
+    return await this.postRepository.postPaginated(params);
+  }
+
   async createPost(data: Prisma.PostCreateInput): Promise<Post> {
-    console.log({ data });
     return await this.postRepository.create(data);
   }
 
   async updatePost(id: number, data: Prisma.PostUpdateInput): Promise<Post> {
     return await this.postRepository.update(id, data);
+  }
+
+  async incrementViews(id: number): Promise<Post> {
+    return await this.postRepository.incrementViews(id);
   }
 
   async deletePost(id: number): Promise<Post> {
--- src/utils/base.service.ts
+++
@@ -1,4 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export abstract class BaseService<T extends { id: number }> {}
